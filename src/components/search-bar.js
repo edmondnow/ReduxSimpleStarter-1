88677999c12bf2959107b36165f299cea060d564
edmondnow <edmond.bitay@gmail.com>
2018-04-23 20:51:33 +0200

    added components

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 
 
 class SearchBar extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {term: ''};
+  }
   render(){
-    return <input onChange ={this.onInputChange} />; 
+    return (
+    <div>
+      <input 
+      value={this.state.term}
+      onChange ={event => this.setState({term:event.target.value})} />
+    </div>
+    );
   }
 
-  onInputChange(){
-    console.log(event.target.value);
-  } 
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { Component }from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search-bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
+
 const API_KEY = require('../config/keys').key;
 
 
-//Create  a new component. This component should produce some HTML
-const App = () => {
-  return (
-  <div>
-    <SearchBar /> 
-  </div>
-  )
 
-}
+//Create  a new component. This component should produce some HTML
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+      //ES6 this.setState({ videos: videos });
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
+} 
 
 
 
 //Take this component's generated HTML and put it
 //on the page (in the DOM)
-
 ReactDOM.render(<App />, document.querySelector('.container')); //when you render I want you to insert the html in this element
